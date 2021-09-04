e5fe33db9cc2c1736c694964a4da1a14b33628f3
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 19:38:15 +0530

    MODIFIED: Saving total wage by company

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -3,35 +3,48 @@ package com.bridgelabz.employeewagecomputation;
 public class EmployeeWageComputation {
 	static final int PART_TIME=1;
 	static final int FULL_TIME=2;
-	static int employeeWageCalculator(String company,int employeeWagePerHour,int noOfWorkingDays,int maximumHoursPerMonth) {
-		int employeeHours=0,employeeWage=0,working_days=0;
-		
-		while(employeeHours<maximumHoursPerMonth && working_days<noOfWorkingDays)
+	private  String COMPANY;
+	private  int EMPLOYEE_RATE_PER_HOUR;
+	private  int NUMBER_OF_WORKING_HOURS;
+	private int MAXIMUM_HOURS_PER_MONTH ;
+	private int TOTAL_WAGE;
+	EmployeeWageComputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth){
+		this.COMPANY = company;
+		this.EMPLOYEE_RATE_PER_HOUR = empRatePerHour;
+		this.NUMBER_OF_WORKING_HOURS = numOfWorkingDays;
+		this.MAXIMUM_HOURS_PER_MONTH = maxHoursInMonth;
+	}
+	public void employeeWageCalculator() {
+		int totalEmployeeHours=0,working_days=0;
+
+		while(totalEmployeeHours<MAXIMUM_HOURS_PER_MONTH && working_days<NUMBER_OF_WORKING_HOURS)
 		{
 			int employeeCheck=(int)Math.floor((Math.random()*10)%3);
 			switch(employeeCheck)
 			{
 			case FULL_TIME:
-				employeeHours+=8;
+				totalEmployeeHours+=8;
 				break;
 			case PART_TIME:
-				employeeHours+=4;
+				totalEmployeeHours+=4;
 				break;
 			default:
 				break;
 			}
 			working_days++;
-			System.out.println("Day#: "+working_days+" Emp Hr: "+employeeHours);
+			System.out.println("Day#: "+working_days+" Emp Hr: "+totalEmployeeHours);
 
 		}			
-		employeeWage=employeeHours*employeeWagePerHour;
-		System.out.println("Total Employee wage for company: "+company+" is "+employeeWage);
-		return employeeWage;
+		TOTAL_WAGE = totalEmployeeHours *EMPLOYEE_RATE_PER_HOUR;
+		System.out.println("Total Employee wage for company: "+COMPANY+" is "+TOTAL_WAGE);
+
 	}
 	public static void main(String[] args) {
 		System.out.println("****** Welcome to Employee Wage Computation ******");
-		employeeWageCalculator("Apple", 20, 2, 10);
-		employeeWageCalculator("Reliance", 10, 4, 20);
-		
+		EmployeeWageComputation apple = new EmployeeWageComputation("APPLE",20, 12, 50);
+		apple.employeeWageCalculator();
+		EmployeeWageComputation Reliance = new EmployeeWageComputation("Reliance",23, 6, 60);
+		Reliance.employeeWageCalculator();
+
 	}
 }
