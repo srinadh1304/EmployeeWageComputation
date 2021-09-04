75fc537366c716c922141492fe9adcd1989e792a
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 10:54:04 +0530

    ADDED: Checking if Employee is present or abscent

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.employeewagecomputation;
 
 public class EmployeeWageComputation {
+	static final int PRESENT=1;
 	public static void main(String[] args) {
 		System.out.println("****** Welcome to Employee Wage Computation ******");
+		int presentOrAbsent=(int)Math.floor((Math.random()*10)%2);
+		if(presentOrAbsent==PRESENT)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is abscent");
+		}
 	}
 }
