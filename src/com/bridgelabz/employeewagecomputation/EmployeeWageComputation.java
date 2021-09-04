12a631ca991aad32d79d8fe7cba0679e79845d13
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 12:48:04 +0530

    MODIFIED: Calculating Employee wage for a month

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -6,23 +6,29 @@ public class EmployeeWageComputation {
 	static final int WAGE_PER_HOUR=20;
 	static final int FULL_DAY_HOUR=8;
 	public static void main(String[] args) {
+		int total_wage=0;
 		System.out.println("****** Welcome to Employee Wage Computation ******");
-		int employeeCheck=(int)Math.floor((Math.random()*10)%3);
-		int employeeHours=0;
-		int employeeWage=0;
-		switch(employeeCheck)
+		for(int working_days=0;working_days<20;working_days++)
 		{
-		case FULL_TIME:
-			employeeHours=8;
-			break;
-		case PART_TIME:
-			employeeHours=4;
-			break;
-		default:
-			employeeHours=0;
-			break;
+			int employeeCheck=(int)Math.floor((Math.random()*10)%3);
+			int employeeHours=0;
+			int employeeWage=0;
+			switch(employeeCheck)
+			{
+			case FULL_TIME:
+				employeeHours=8;
+				break;
+			case PART_TIME:
+				employeeHours=4;
+				break;
+			default:
+				employeeHours=0;
+				break;
+			}
+			employeeWage=employeeHours*WAGE_PER_HOUR;
+			total_wage+=employeeWage;
+			
 		}
-		employeeWage=employeeHours*WAGE_PER_HOUR;
-		System.out.println("Employee wage : "+employeeWage);
+		System.out.println("Employee wage for a  month : "+total_wage);
 	}
 }
