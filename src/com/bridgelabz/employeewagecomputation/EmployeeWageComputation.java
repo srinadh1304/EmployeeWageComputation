c0a8a06748edea98bfda7310b4a3cee5cdf27f62
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 11:08:35 +0530

    MODIFIED: Checking if employee is working part time or full time

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -1,17 +1,22 @@
 package com.bridgelabz.employeewagecomputation;
 
 public class EmployeeWageComputation {
-	static final int PRESENT=1;
+	static final int PART_TIME=1;
+	static final int FULL_TIME=2;
 	static final int WAGE_PER_HOUR=20;
 	static final int FULL_DAY_HOUR=8;
 	public static void main(String[] args) {
 		System.out.println("****** Welcome to Employee Wage Computation ******");
-		int presentOrAbsent=(int)Math.floor((Math.random()*10)%2);
+		int employeeCheck=(int)Math.floor((Math.random()*10)%3);
 		int employeeHours=0;
 		int employeeWage=0;
-		if(presentOrAbsent==PRESENT)
+		if(employeeCheck==FULL_TIME)
 		{
 			employeeHours=8;
+		}
+		else if(employeeCheck==PART_TIME)
+		{
+			employeeHours=4;
 		}
 		else
 		{
