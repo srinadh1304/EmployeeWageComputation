8192415aa59bcff1271aed134ab7e2dc3751fa5f
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 12:56:19 +0530

    MODIFIED: Calculating employee wage till maximum hours or days is reached

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -4,31 +4,30 @@ public class EmployeeWageComputation {
 	static final int PART_TIME=1;
 	static final int FULL_TIME=2;
 	static final int WAGE_PER_HOUR=20;
-	static final int FULL_DAY_HOUR=8;
+	static final int FULL_WORKING_HOURS=100;
+	static final int TOTAL_WORKING_DAYS=20;
 	public static void main(String[] args) {
-		int total_wage=0;
+		int employeeHours=0,employeeWage=0,working_days=0;
 		System.out.println("****** Welcome to Employee Wage Computation ******");
-		for(int working_days=0;working_days<20;working_days++)
+		while(employeeHours<FULL_WORKING_HOURS && working_days<TOTAL_WORKING_DAYS)
 		{
 			int employeeCheck=(int)Math.floor((Math.random()*10)%3);
-			int employeeHours=0;
-			int employeeWage=0;
 			switch(employeeCheck)
 			{
 			case FULL_TIME:
-				employeeHours=8;
+				employeeHours+=8;
 				break;
 			case PART_TIME:
-				employeeHours=4;
+				employeeHours+=4;
 				break;
 			default:
-				employeeHours=0;
 				break;
 			}
-			employeeWage=employeeHours*WAGE_PER_HOUR;
-			total_wage+=employeeWage;
-			
-		}
-		System.out.println("Employee wage for a  month : "+total_wage);
+			working_days++;
+
+
+		}			
+		employeeWage=employeeHours*WAGE_PER_HOUR;
+		System.out.println("Employee wage for a  month : "+employeeWage);
 	}
 }
